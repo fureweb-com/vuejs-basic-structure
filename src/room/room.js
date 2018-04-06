7507fea5c81ece541cf7168d53ba59bf1a60b151
fureweb-com <fureweb.com@gmail.com>
2018-04-06 17:43:56 +0900

    Update room.js
    - call req2svr's test method in room component

--- src/room/room.js
+++ src/room/room.js
@@ -1,7 +1,17 @@
+import req2svr from './req2svr'
+
 export default {
   name: 'app-room',
   data() {
-    return {}
+    return {
+      list: []
+    }
+  },
+  mounted() {
+    req2svr.test().then(
+      r => { this.list = r },
+      err => console.error(err)
+    )
   },
   templateSrc: './room.html',
   styleSrc: './room.css'
